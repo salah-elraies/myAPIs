16db8a25da2bac70a331f08e84e05a9d53f5d8a7
Salah Elraies <s4l47.elraies@gmail.com>
2021-09-11 01:05:49 +0200

    fixing cors with express doc function lol

--- server.js
+++ server.js
@@ -17,12 +17,10 @@ const port = process.env.PORT || 8001;
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://al7deedy-pipes-and-fittings.web.app"
-  ); // update to match the domain you will make the request from
-  res.header(
+    "https://al7deedy-pipes-and-fittings.web.app",
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  ); // update to match the domain you will make the request from
   next();
 });
 app.use(express.json());
