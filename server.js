b3f1198254676e82929151e2a209f5ffd05bbfd3
Salah Elraies <s4l47.elraies@gmail.com>
2021-09-11 01:21:51 +0200

    fixing cors with express doc server cors cry

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ const port = process.env.PORT || 8001;
 
 // middlwares
 app.use(Cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use("/products", productsRouter);
 
